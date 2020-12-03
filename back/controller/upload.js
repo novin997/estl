3c61299b15d1b2a0a64907ce4db897899a1fac59
novin997 <cocolupiayi@gmail.com>
2020-12-03 20:33:57 +0800

    read csv file in nodejs

--- back/controller/upload.js
+++ back/controller/upload.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const multer = require("multer");
 
 const route = express.Router();
+const upload = multer();
 
-route.post("/upload", (req, res) => {
-  console.log("test api");
+route.post("/upload", upload.single("uploadCSV"), (req, res) => {
+  //   console.log(req.files.file.data);
+  const csv = req.files.file.data.toString("utf8");
+  console.log(csv);
   res.json({ msg: "Good result" });
 });
 
